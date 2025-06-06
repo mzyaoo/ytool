f1acaa419e94a424d9fa06112e78c131e9d5ebd3
imzyao <13593554644@163.com>
2025-06-06 13:35:22 +0800

    线程池配置

---
+++ src/main/java/com/mzyao/ytool/config/AsyncThreadConfig.java
@@ -0,0 +1,29 @@
+package com.mzyao.ytool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * 异步线程池
+ */
+@Configuration
+@EnableAsync
+public class AsyncThreadConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.initialize();
+        return executor;
+    }
+
+}
+
+
